232ee187ac33819a5c0c4e66b36ec4d7a3dccd4e
rladydwp88 <rladydwp88@gmail.com>
2025-02-15 16:15:30 +0900

    공부

--- 03_control/src/edu/kh/control/practice/LoopPractice.java
+++ 03_control/src/edu/kh/control/practice/LoopPractice.java
@@ -3,67 +3,41 @@ package edu.kh.control.practice;
 import java.util.Scanner;
 
 public class LoopPractice {
-
+	
 	Scanner sc = new Scanner(System.in);
 	
-	public void practice1() {
+	public void practice1 () {
 		
 		System.out.print("1이상의 숫자를 입력하세요 : ");
 		int input = sc.nextInt();
+		String result = " ";
 		
-		if(input >= 1) {
-			
-			for(int i =1; i <= input; i++) {
-				System.out.println(i + " ");
+		if ( input > 1 ) {
+			for (int i = 1; i <= input; i++) {
+				System.out.print(i + result);
 			}
 			
 		} else {
-			System.out.println("1 이상의 숫자를 입력하세요");
+			System.out.println("1 이상의 숫자를 입력해주세요.");
 		}
 		
-		/*
-		System.out.print("1이상의 숫자를 입력하세요 : ");
-		int num = sc.nextInt();
-		
-		for (int i = 1; i <= num; i++) {
-			System.out.print(i + " ");
-		}
-		
-		if (1 > num) {
-			System.out.print("1 이상의 숫자를 입력하세요");
-		}
-		*/
 	}
 	
 	public void practice2() {
 		
 		System.out.print("1이상의 숫자를 입력하세요 : ");
 		int input = sc.nextInt();
+		String result = " ";
 		
-		if(input >= 1) {
-			
-			for(int i = input; i >= 1; i--) {
-				System.out.println(i + " ");
+		if (input >= 1) {
+			for (int i = input; i >= 1; i--) {
+				System.out.print(i + result);
 			}
 			
 		} else {
-			System.out.println("1 이상의 숫자를 입력하세요");
+			System.out.println("1 이상의 숫자를 입력해주세요");
 		}
 		
-		
-		/*
-		System.out.print("1이상의 숫자를 입력하세요 : ");
-		int num = sc.nextInt();
-		
-		for (int i = num; i > 0; i--) {
-			System.out.print(i + " ");
-			
-		}
-		
-		if (1 > num) {
-			System.out.print("1 이상의 숫자를 입력하세요");
-		}
-		*/
 	}
 	
 	public void practice3() {
@@ -73,101 +47,93 @@ public class LoopPractice {
 		
 		int sum = 0;
 		
-		for(int i = 1; i <= input; i++) {
+		for (int i = 1; i <= input; i++) {
+			
 			sum += i;
 			
-			if(i == input) {
-				
+			if (i < input) {
+				System.out.print(i + " + ");
+			} else {
 				System.out.print(i + " = " + sum);
+			}
+			
+		}
+		
+	}
+	
+	public void practice4() {
+
+		System.out.print("첫 번째 숫자 : ");
+		int input1 = sc.nextInt();
+
+		System.out.print("두 번째 숫자 : ");
+		int input2 = sc.nextInt();
+
+		if (input1 < 1 || input2 < 1) {
+			System.out.println("1 이상의 숫자를 입력해주세요.");
+		} else {
+
+			int start = input1;
+			int end = input2;
+
+			if (input1 > input2) {
+				start = input2;
+				end = input1;
+			}
+
+			for (int i = start; i <= end; i++) {
+				System.out.print(i + " ");
+			}
+
+		}
+
+		/*
+		for (int i = 1; i >= input1 || i <= input2; i++) {
+			
+			if (input1 > input2) {
 				
 			} else {
-				
-				System.out.print(i + " + ");
-				
+				System.out.println(i);
 			}
 			
 		}
-		
-		/*
-		System.out.print("정수를 하나 입력하세요 : ");
-		int num = sc.nextInt();
+		*/
+	}
 	
-		int sum1 = 0;
-		String result = "";
-		
-		for (int i = 1; i < num; i++ ) {
-			
-			result += i + " + ";
-			sum1 += i;
-			
-		}
-		
-		int sum2 = sum1 + num;
-		
-		System.out.printf("%s" + num + " = %d", result, sum2);
-		
-		*/
-	}
-
-	public void practice4() {
-		
-		System.out.print("첫 번째 숫자 : ");
-		int num1 = sc.nextInt();
-		
-		System.out.print("두 번째 숫자 : ");
-		int num2 = sc.nextInt();
-		
-		if(num1 < 1 || num2 < 1) {
-			System.out.println("1 이상의 숫자만 입력");
-			
-		} else {
-			
-			int start = num1;
-			int end = num2;
-			
-			if (num1 > num2) {
-				
-				start = num2;
-				end = num1;
-				
-			}
-			
-			for(int i = start; i <= end; i++) {
-				
-				System.out.println(i + " ");
-				
-			}
-			
-		}
+	public void practice5() {
 		
 		/*
-		System.out.print("첫 번째 숫자 : ");
-		int num1 = sc.nextInt();
-		
-		System.out.print("두 번째 숫자 : ");
-		int num2 = sc.nextInt();
-		
-		for (int i = 1; i >= num1 && i <= num2; i++) {
-			
-			System.out.println(i);
-			// if (i <= num1 && i >= num2) {
-		}
-		*/
-	}
-
-	public void practice7() {
-		
-		// - 행(row)은 입력한 input 만큼
-		// - 열(col)은 현재행 (row)만큼 반복
-		
-		System.out.println("정수 입력 : ");
+		System.out.print("숫자 : ");
 		int input = sc.nextInt();
 		
-		for(int row = 1; row <=input; row++) {
+		int sum = 0;
+		
+		System.out.println("===== " + input + "단" + " ====");
+		
+		for (int i = 1; i <= 9; i++) {
+			
+			sum = input * i;
+			
+			System.out.println(input + " * " + i + " = " + sum);
+			
 			
 		}
-				
-				
+		*/
+		
+		System.out.print("숫자 : ");
+		int input = sc.nextInt();
+		
+		System.out.println("===== " + input + "단 ====");
+		
+		for (int i = 1; i <= 9; i++) {
+			
+			System.out.printf("%d x %d = %d\n", input, i, input * i );
+			
+		}
+		
+	}
+	
+	public void practice6() {
 		
 	}
 	
--- 03_control/src/edu/kh/control/practice/Run2.java
+++ 03_control/src/edu/kh/control/practice/Run2.java
@@ -4,12 +4,14 @@ public class Run2 {
 
 	public static void main(String[] args) {
 
-		LoopPractice lp = new LoopPractice(); 
-
-		// lp.practice1 ();
-		// lp.practice2 ();
-		lp.practice3 ();
-	   	// lp.practice4 ();
+	LoopPractice lp = new LoopPractice();
+		
+		// lp.practice1();
+		// lp.practice2();
+		// lp.practice3();
+		// lp.practice4();
+		// lp.practice5();
+		lp.practice6();
 		
 	}
 
