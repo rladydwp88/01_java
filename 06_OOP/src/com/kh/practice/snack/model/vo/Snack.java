56340f8e245a475e56941a3dcf254b3be69df23b
rladydwp88 <rladydwp88@gmail.com>
2025-02-23 20:57:08 +0900

    집

--- 06_OOP/src/com/kh/practice/snack/model/vo/Snack.java
+++ 06_OOP/src/com/kh/practice/snack/model/vo/Snack.java
@@ -1,54 +1,70 @@
 package com.kh.practice.snack.model.vo;
 
+// vo : 값 저장용 객체
 public class Snack {
-	
-	private String kind;
+	// 필드
+	private String kind; 
 	private String name;
-	private String flavor;
+	private String flavor; 
 	private int numOf;
-	private int Price;
+	private int price;
 	
+	// 기본생성자
 	public Snack() {}
-	
+
+	// 매개변수 생성자
 	public Snack(String kind, String name, String flavor, int numOf, int price) {
 		this.kind = kind;
 		this.name = name;
 		this.flavor = flavor;
 		this.numOf = numOf;
-		this.Price = price;
+		this.price = price;
 	}
 
+
+	// 메서드
+	// getter/setter
 	public String getKind() {
 		return kind;
 	}
+
 	public void setKind(String kind) {
 		this.kind = kind;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getFlavor() {
 		return flavor;
 	}
+
 	public void setFlavor(String flavor) {
 		this.flavor = flavor;
 	}
+
 	public int getNumOf() {
 		return numOf;
 	}
+
 	public void setNumOf(int numOf) {
 		this.numOf = numOf;
 	}
+
 	public int getPrice() {
-		return Price;
+		return price;
 	}
+
 	public void setPrice(int price) {
-		Price = price;
+		this.price = price;
 	}
 	
-	public String information;
-
+	public String infomation() {
+		return kind + "(" + name + " - " + flavor + ") " + numOf + "개 " + price + "원";
+	}
 }
--- 06_OOP/src/com/kh/practice/snack/run/Run.java
+++ 06_OOP/src/com/kh/practice/snack/run/Run.java
@@ -3,12 +3,8 @@ package com.kh.practice.snack.run;
 import com.kh.practice.snack.view.SnackMenu;
 
 public class Run {
-
 	public static void main(String[] args) {
-		
-		SnackMenu snmenu = new SnackMenu();
-		snmenu.menu();
-
+		SnackMenu sm = new SnackMenu();
+		sm.menu(); // menu() 호출
 	}
-
 }
